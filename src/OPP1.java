02d10517ce24e4f2781a6063b698dd87327f0679
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 19:10:50 -0300

    add some stuff

--- src/OPP1.java
+++ src/OPP1.java
@@ -1,8 +1,13 @@
 public class OPP1 {
     public static void main(String[] args) {
         Animal animal = new Animal("joseph", 20);
+        System.out.println(Animal.getAnimalCount());
+
         Cat gato = new Cat("uwu", 23);
+        System.out.println(Cat.getAnimalCount());
+
         Dog perro = new Dog("Firu", 10);
+        System.out.println(Dog.getAnimalCount());
 
         System.out.println(animal.animalSound());
         System.out.println(gato.animalSound());
