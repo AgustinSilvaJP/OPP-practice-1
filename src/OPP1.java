79d07348b2b7a0e19fa2e3ef7aced51793d7f5fd
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 16:12:12 -0300

    add some code to test the new Person class functionality

--- src/OPP1.java
+++ src/OPP1.java
@@ -7,6 +7,18 @@ public class OPP1 {
         p1.age = 23;
         p1.isAlive = true;
 
-        System.out.printf("Person 1 name: %s, subname: %s, age: %d, alive?: %b\n", p1.name, p1.subName, p1.age, p1.isAlive);
+        Persona p2 = new Persona();
+        p2.name = "suga";
+        p2.subName = "nit";
+        p2.age = 20;
+        p2.isAlive = false;
+
+        System.out.printf("Person 1: Name & subname: %s, age: %d, alive?: %b\n", p1.getName(), p1.age, p1.isAlive);
+
+        System.out.println(p1.saluteTo(p1.getName()));
+
+        System.out.println();
+        System.out.printf("Person 2: Name & subname: %s, age: %d, alive?: %b\n", p2.getName(), p2.age, p2.isAlive);
+        System.out.println(p2.saluteTo(p2.getName()));
     }
 }
