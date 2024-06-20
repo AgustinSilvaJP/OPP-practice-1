08cdb928a64f087bfe46262dccea5c7a31b28b56
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 16:11:28 -0300

    add method 'getname() & saluteTo(str name)' to the Person class

--- src/Persona.java
+++ src/Persona.java
@@ -1,8 +1,19 @@
 public class Persona {
-    //atributos
+    //atributos (estado/caracteristicas de un objeto);
     public String name;
     public String subName;
     public int age;
     public boolean isAlive;
 
+    //metodos (comportamientos de un objeto)
+
+    public String getName(){
+        return name + " " + subName;
+    }
+
+    public String saluteTo(String personToSalute){
+        if(isAlive) return "Good day " + personToSalute + " how are you?";
+
+        return personToSalute + " is dead... F";
+    }
 }
