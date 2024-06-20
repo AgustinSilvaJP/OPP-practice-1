ccb090a74ae020ff410ad568bacd17b81847003e
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 18:38:04 -0300

    super clase Animal

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String animalSound(){
+        return "a generic animal sound :v";
+    }
+}
