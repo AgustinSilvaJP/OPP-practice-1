f970799cac3e33874bf5b23485eb4be3470a53f2
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 18:42:22 -0300

    some modifications here and there

--- src/OPP1.java
+++ src/OPP1.java
@@ -1,15 +1,11 @@
 public class OPP1 {
     public static void main(String[] args) {
-        Persona p1 = new Persona("agus", "silva", 5, true);
+        Animal animal = new Animal("joseph", 20);
+        Cat gato = new Cat("uwu", 23);
+        Dog perro = new Dog("Firu", 10);
 
-        System.out.printf("Person 1: Name & subname: %s, age: %d, alive?: %b\n", p1.getName(), p1.age, p1.isAlive);
-        System.out.println(p1.saluteTo(p1.getName()));
-        System.out.println();
-
-        Persona p2 = new Persona("agus", 23);
-        System.out.println(p2.saluteTo(p2.getName()));
-        p2.subName = "xd";
-
-        System.out.println(p2.saluteTo(p2.getName()));
+        System.out.println(animal.animalSound());
+        System.out.println(gato.animalSound());
+        System.out.println(perro.animalSound());
     }
 }
