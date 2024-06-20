e8af4c6b7cfe9660d3ad85b90e423b5b8e609d41
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 19:10:28 -0300

    add a static variable that counts the objects instantiated

--- src/Animal.java
+++ src/Animal.java
@@ -1,13 +1,17 @@
 public class Animal {
     String name;
     int age;
+    static int animalCount = 0;
 
     public Animal(String name, int age){
         this.name = name;
         this.age = age;
+        animalCount++;
     }
-
     public String animalSound(){
         return "a generic animal sound :v";
+    }
+    public static int getAnimalCount(){
+        return animalCount;
     }
 }
