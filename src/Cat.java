399378c28c33907a08de41427fd7182ae2bc3625
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 18:38:27 -0300

    clases que heredan de Animal

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Cat {
+//extends especifica herencia de Cat por parde de la clase Animal
+public class Cat extends Animal{
+
+
+    public Cat(String name, int age){
+        //con keyword super estamos asignando informacion heredada
+        super(name, age);
+    }
+
+    @Override
+    public String animalSound(){
+        return "nyaaa uwu";
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog extends Animal{
+
+    public Dog(String name, int age){
+        super(name, age);
+    }
+
+    @Override
+    public String animalSound(){
+        return "guau guau... im a dog...";
+    }
 }
