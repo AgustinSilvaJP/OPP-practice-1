649455b15b235dbded23c8af5f26e025b6d2eeee
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 17:22:28 -0300

    a

--- src/OPP1.java
+++ src/OPP1.java
@@ -28,10 +28,12 @@ public class OPP1 {
         System.out.printf("Person 1: Name & subname: %s, age: %d, alive?: %b\n", p1.getName(), p1.age, p1.isAlive);
         System.out.println(p1.saluteTo(p1.getName()));
         System.out.println(p1.getMajorInfo());
+        System.out.println(p1.major.duration);
         System.out.println();
 
         System.out.printf("Person 2: Name & subname: %s, age: %d, alive?: %b\n", p2.getName(), p2.age, p2.isAlive);
         System.out.println(p2.saluteTo(p2.getName()));
         System.out.println(p2.getMajorInfo());
+        System.out.println(p2.major.majorName);
     }
 }
