a9e998025ffc2764aa0c6d552c195c06958ba6df
Agus <AgustinSilvaJP@gmail.com>
2024-06-20 16:27:27 -0300

    add attr major of type Major, and a new method 'getMajor' that return a String with the info of the person study info

--- src/Persona.java
+++ src/Persona.java
@@ -4,16 +4,18 @@ public class Persona {
     public String subName;
     public int age;
     public boolean isAlive;
+    Major major;
 
     //metodos (comportamientos de un objeto)
 
     public String getName(){
         return name + " " + subName;
     }
-
+    public String getMajorInfo(){
+        return major.majorName + ", duration: " + major.duration + " years, in course: " + major.inCourse;
+    }
     public String saluteTo(String personToSalute){
         if(isAlive) return "Good day " + personToSalute + " how are you?";
-
         return personToSalute + " is dead... F";
     }
 }
